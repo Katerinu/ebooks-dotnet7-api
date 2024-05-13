8ed7ac5cd06262f8b6c48230978cf934765dc6f4
VicenteeFigueroa <vicente.figueroa01@alumnos.ucn.cl>
2024-05-13 14:52:31 -0400

    feat: added swagger services.

--- Program.cs
+++ Program.cs
@@ -3,9 +3,26 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+//services
 builder.Services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("ebooks"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Nombre de tu API", Version = "v1" });
+});
+
+
+
 var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ebok-API");
+});
+
+
 
 var ebooks = app.MapGroup("api/ebook");
 
