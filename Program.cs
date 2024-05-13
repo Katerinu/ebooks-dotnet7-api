fd23ba2c332f23f3b0bd2a7f4c9923c71b899177
VicenteeFigueroa <vicente.figueroa01@alumnos.ucn.cl>
2024-05-13 15:13:17 -0400

    feat: implemented all endpoints

--- Program.cs
+++ Program.cs
@@ -10,12 +10,13 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Nombre de tu API", Version = "v1" });
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Ebook-API", Version = "v1" });
 });
 
 
 
 var app = builder.Build();
+//swagger endpoints and settings.
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
@@ -28,6 +29,12 @@ var ebooks = app.MapGroup("api/ebook");
 
 // TODO: Add more routes
 ebooks.MapPost("/", CreateEBookAsync);
+ebooks.MapGet("/", GetAllEbooksAsync);
+ebooks.MapPut("/{id}", UpdateBookAsync);
+ebooks.MapPut("/{id}/change-availability", ChangeAvailabilitysync);
+ebooks.MapPut("{id}/increment-stock", IncrementStockAsync);
+ebooks.MapPost("/purchase", EbookPurchaseAsync);
+ebooks.MapDelete("/{id}", DeleteEbookAsync);
 
 app.Run();
 
@@ -36,3 +43,34 @@ async Task<IResult> CreateEBookAsync(DataContext context)
 {
     return Results.Ok();
 }
+
+async Task<IResult> GetAllEbooksAsync([FromQuery] string genre, [FromQuery] string author, [FromQuery] string format, DataContext context)
+{
+    return Results.Ok();
+}
+
+async Task<IResult> UpdateBookAsync(int id, DataContext context)
+{
+    return Results.Ok();
+}
+
+async Task<IResult> ChangeAvailabilitysync(int id, DataContext context)
+{
+    return Results.Ok();
+}
+
+async Task<IResult> IncrementStockAsync(int id, DataContext context)
+{
+    return Results.Ok();
+}
+
+async Task<IResult> EbookPurchaseAsync(DataContext context)
+{
+    return Results.Ok();
+}
+
+async Task<IResult> DeleteEbookAsync(int id, DataContext context)
+{
+    return Results.Ok();
+}
+
